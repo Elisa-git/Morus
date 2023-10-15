827af52c218bed74e40daaa878632c49f9f675ae
Daniel Pereira <daniel.pereira@codenapp.com>
2023-10-15 14:12:52 -0300

    fix

--- Morus.API/Models/InformacaoRequest.cs
+++ Morus.API/Models/InformacaoRequest.cs
@@ -2,9 +2,11 @@
 {
     public class InformacaoRequest
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public string Titulo { get; set; }
         public string Descricao { get; set; }
         public bool Ativo { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public DateTime DataAlteracao { get; set; }
     }
 }
--- Morus.API/Models/OcorrenciaRequest.cs
+++ Morus.API/Models/OcorrenciaRequest.cs
@@ -2,7 +2,7 @@
 {
     public class OcorrenciaRequest
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public string Titulo { get; set; }
         public string Descricao { get; set; }
         public DateTime DataCadastro { get; set; }
