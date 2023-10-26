e928316e9a373732d0c2581792c652ef2e1fdfb1
Daniel Pereira <daniel.pereira@codenapp.com>
2023-10-26 17:07:57 -0300

    fix livro caixa

--- Morus.API/Controllers/LivroCaixaController.cs
+++ Morus.API/Controllers/LivroCaixaController.cs
@@ -24,6 +24,7 @@ namespace Morus.API.Controllers
         {
             _livroCaixaService = livroCaixaService;
             _livroCaixaApplication = livroCaixaApplication;
+            this.mapper = mapper;
         }
 
         [Authorize]
